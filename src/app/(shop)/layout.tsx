a0688e22384aafb1a31c6367169a8c5a6cc5a5fb
richard salazar <richardrocker91@gmail.com>
2026-02-03 21:21:23 -0500

    Eliminar marca de agua en footer

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -14,7 +14,7 @@ export default function ShopLayout({
             <footer className="border-t py-6 md:px-8 md:py-0">
                 <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
                     <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
-                        Built by Antigravity.
+                        © {new Date().getFullYear()} Center Tecno.
                     </p>
                 </div>
             </footer>
